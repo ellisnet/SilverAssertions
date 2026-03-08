1b89869ffef3485903e84e76e7ff19d520560329
Jeremy Ellis <jeremy@ellisnet.com>
2026-03-07 23:17:50 -0800

    Added third party notices.

--- tests/SilverAssertions.Tests/Execution/CallerIdentifierTests.cs
+++ tests/SilverAssertions.Tests/Execution/CallerIdentifierTests.cs
@@ -36,6 +36,8 @@ namespace SilverAssertions.Tests.Execution
             act.Should().Throw<XunitException>().WithMessage("Expected function to be*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_namespace_is_prefixed_with_System_caller_should_be_known()
         {
@@ -46,6 +48,8 @@ namespace SilverAssertions.Tests.Execution
             act.Should().Throw<XunitException>().WithMessage("Expected actualCaller to be*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_variable_name_contains_Should_it_should_identify_the_entire_variable_name_as_the_caller()
         {
@@ -60,6 +64,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected fooShould to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public async Task When_should_is_passed_argument_context_should_still_be_found()
         {
@@ -77,6 +83,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Did not expect bob to complete within 1s because test testArg.");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_variable_is_captured_it_should_use_the_variable_name()
         {
@@ -91,6 +99,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_field_is_the_subject_it_should_use_the_field_name()
         {
@@ -106,6 +116,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.Field to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_property_is_the_subject_it_should_use_the_property_name()
         {
@@ -120,6 +132,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.Bar to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_method_name_is_the_subject_it_should_use_the_method_name()
         {
@@ -134,6 +148,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod() to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_method_contains_arguments_it_should_add_them_to_caller()
         {
@@ -148,6 +164,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\"test\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_the_caller_contains_multiple_members_it_should_include_them_all()
         {
@@ -166,6 +184,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.GetFoo(test1).GetFooStatic(\"test\" + 2).GetFoo(foo.Field) to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_the_caller_contains_multiple_members_across_multiple_lines_it_should_include_them_all()
         {
@@ -189,6 +209,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.GetFoo(test1).GetFooStatic(\"test\" + 2).GetFoo(foo.Field) to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_Should_it_should_include_that_to_the_caller()
         {
@@ -203,6 +225,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\".Should()\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_semicolon_it_should_include_that_to_the_caller()
         {
@@ -217,6 +241,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\"test;\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         [SuppressMessage("Code should not contain multiple statements on one line", "SA1107")]
         [SuppressMessage("Code should not contain multiple statements on one line", "IDE0055")]
@@ -238,6 +264,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo2 to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_escaped_quote_it_should_include_that_to_the_caller()
         {
@@ -252,6 +280,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\"test\\\";\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_verbatim_string_it_should_include_that_to_the_caller()
         {
@@ -266,6 +296,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(@\"test\", argument2: $@\"test2\", argument3: @$\"test3\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_multi_line_verbatim_string_it_should_include_that_to_the_caller()
         {
@@ -284,6 +316,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(@\"            bob dole            \") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_verbatim_string_interpolation_it_should_include_that_to_the_caller()
         {
@@ -298,6 +332,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(@$\"test\"\";\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_concatenations_it_should_include_that_to_the_caller()
         {
@@ -312,6 +348,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\"1\" + 2) to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_multi_line_concatenations_it_should_include_that_to_the_caller()
         {
@@ -329,6 +367,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\"abc\"+ \"def\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_string_with_comment_like_contents_inside_it_should_include_them_to_the_caller()
         {
@@ -343,6 +383,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(\"test//test2/*test3*/\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_arguments_contain_verbatim_string_with_verbatim_like_string_inside_it_should_include_them_to_the_caller()
         {
@@ -357,6 +399,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.BarMethod(@\"test @\"\" @$\"\" bob\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         [SuppressMessage("Single - line comment should be preceded by blank line", "SA1515")]
         [SuppressMessage("Single - line comment should be preceded by blank line", "IDE0055")]
@@ -378,6 +422,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         [SuppressMessage("Single - line comment should be preceded by blank line", "SA1515")]
         [SuppressMessage("Single - line comment should be preceded by blank line", "IDE0055")]
@@ -401,6 +447,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         [SuppressMessage("Single - line comment should be preceded by blank line", "SA1515")]
         [SuppressMessage("Single - line comment should be preceded by blank line", "IDE0055")]
@@ -422,6 +470,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         [SuppressMessage("Code should not contain multiple statements on one line", "IDE0055")]
         [SuppressMessage("Single-line comment should be preceded by blank line", "SA1515")]
@@ -446,6 +496,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.GetFoo(\"bob\").BarMethod() to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void When_the_method_has_Should_prefix_it_should_read_whole_method()
         {
@@ -463,6 +515,8 @@ namespace SilverAssertions.Tests.Execution
         [Collection("UIFacts")]
         public class UIFacts
         {
+            //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+            //  it will likely fail in Release configuration. Run in Debug configuration.
             [Fact] //[UIFact] //TODO: Decide if I still need this test to run using the Xunit.StaFact NuGet package
             public async Task Caller_identification_should_also_work_for_statements_following_async_code()
             {
@@ -480,6 +534,8 @@ namespace SilverAssertions.Tests.Execution
             }
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void A_method_taking_an_array_initializer_is_an_identifier()
         {
@@ -496,6 +552,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.GetFoo(new[] { 1, 2, 3 }.Sum() + \"\") to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void A_method_taking_a_target_typed_new_expression_is_an_identifier()
         {
@@ -512,6 +570,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected foo.GetFoo(new('a', 10)) to be <null>*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void A_method_taking_a_list_is_an_identifier()
         {
@@ -550,6 +610,8 @@ namespace SilverAssertions.Tests.Execution
                 .WithMessage("Expected object to be*");
         }
 
+        //NOTE: This test relies on CallerIdentifier reading source file info from the stack trace (PDB);
+        //  it will likely fail in Release configuration. Run in Debug configuration.
         [Fact]
         public void All_core_code_anywhere_in_the_stack_trace_is_ignored()
         {
